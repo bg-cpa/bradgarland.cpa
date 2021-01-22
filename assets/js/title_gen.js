455f88d452f9f548ef235d3d6e62630356f8e0fe
bg-cpa <77604391+bg-cpa@users.noreply.github.com>
2021-01-22 01:09:23 -0600

    Update title_gen.js

--- assets/js/title_gen.js
+++ assets/js/title_gen.js
@@ -1,6 +1,6 @@
-var word1_list = ["civic","creative","organized","adopted","poetic","local","reluctant","community",]
+var word1_list = ["enterprising","creative","number-crunching","adopted","poetic","local","community","collaborative","caffeinated"]
 var word1 = word1_list[Math.floor(Math.random() * word1_list.length)];
-var word2_list = ["leader","organizer","creative","accountant","innovator","Huntsvillian","professional"]
+var word2_list = ["learner","organizer","creator","accountant","innovator","Huntsvillian","professional","entrepreneur","bean counter"]
 var word2 = word2_list[Math.floor(Math.random() * word2_list.length)];	
 
 document.getElementById("title").innerHTML = word1 + " " + word2;

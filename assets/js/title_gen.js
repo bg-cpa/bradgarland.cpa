824d645c0a8040c610885a29381384998398ac31
bg-cpa <77604391+bg-cpa@users.noreply.github.com>
2021-01-23 21:43:42 -0600

    Update title_gen.js

--- assets/js/title_gen.js
+++ assets/js/title_gen.js
@@ -1,6 +1,6 @@
-var word1_list = ["enterprising","creative","number-crunching","adopted","poetic","local","community","collaborative","caffeinated"]
+var word1_list = ["enterprising","creative","number-crunching","adopted","poetic","local","community","collaborative","caffeinated","nerdy","Deep South"]
 var word1 = word1_list[Math.floor(Math.random() * word1_list.length)];
-var word2_list = ["learner","organizer","creator","accountant","innovator","Huntsvillian","professional","entrepreneur","bean counter"]
+var word2_list = ["learner","organizer","creator","accountant","innovator","Huntsvillian","professional","entrepreneur","bean counter","Korean","nerd"]
 var word2 = word2_list[Math.floor(Math.random() * word2_list.length)];	
 
 document.getElementById("title").innerHTML = word1 + " " + word2;

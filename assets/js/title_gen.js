5ec36db16fa513eadff1ffa256b04a7e40a3c211
bg-cpa <77604391+bg-cpa@users.noreply.github.com>
2021-02-27 21:22:57 -0600

    Update title_gen.js

--- assets/js/title_gen.js
+++ assets/js/title_gen.js
@@ -1,6 +1,6 @@
-var word1_list = ["enterprising","creative","number-crunching","adopted","poetic","local","community","collaborative","caffeinated","nerdy","Deep South"]
+var word1_list = ["enterprising","creative","number-crunching","adopted","poetic","local","community","collaborative","caffeinated","nerdy","Deep South","Southern","introverted"]
 var word1 = word1_list[Math.floor(Math.random() * word1_list.length)];
-var word2_list = ["learner","organizer","creator","accountant","innovator","Huntsvillian","professional","entrepreneur","bean counter","Korean","nerd"]
+var word2_list = ["learner","organizer","creator","accountant","innovator","Huntsvillian","professional","entrepreneur","bean counter","Korean","nerd","husband","father"]
 var word2 = word2_list[Math.floor(Math.random() * word2_list.length)];	
 
 document.getElementById("title").innerHTML = word1 + " " + word2;
